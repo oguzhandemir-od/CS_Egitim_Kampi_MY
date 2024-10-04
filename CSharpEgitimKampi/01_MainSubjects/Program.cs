b151f4c622851108df4b4416847374bfdf063250
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-04 17:32:55 +0300

    Integer Variables

--- CSharpEgitimKampi/01_MainSubjects/Program.cs
+++ CSharpEgitimKampi/01_MainSubjects/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -68,6 +69,74 @@ namespace _01_MainSubjects
             //Console.WriteLine("Email Adresi: " + customerEmail);
             //Console.WriteLine("Adres: " + customerDistrict + "/" + customerCity);
             //Console.WriteLine("------------------------------------------------");
+            #endregion
+
+            #region Int Değişkenler
+
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("**** Restoran Menü Fiyatları ****");
+            Console.WriteLine();
+            Console.WriteLine("----Hamburger: " + hamburgerPrice + " TL");
+            Console.WriteLine("----Pizza: " + pizzaPrice + " TL");
+            Console.WriteLine("----Kola: " + cokePrice + " TL");
+            Console.WriteLine("----Limonata: " + lemonadePrice + " TL");
+            Console.WriteLine("----Kızartma: " + friesPrice);
+            Console.WriteLine("----Su: " + waterPrice + " TL");
+            Console.WriteLine();
+            Console.WriteLine("**** Tutar ****");
+
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalHamburgerPrice = 0;
+            int totalCokePrice = 0;
+            int totalWaterPrice = 0;
+            int totalFriesPrice = 0;
+            int totalPizzaPrice = 0;
+            int totalLemonadePrice = 0;
+
+
+            hamburgerCount = 3;
+            cokeCount=3;
+            waterCount=3;
+            friesCount = 1;
+            pizzaCount = 0;
+            lemonadeCount = 0;
+
+            totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            totalCokePrice = cokeCount * cokePrice;
+            totalWaterPrice = waterCount * waterPrice;
+            totalFriesPrice = friesCount * friesPrice;
+            totalPizzaPrice = pizzaCount * pizzaPrice;
+            totalLemonadePrice = lemonadeCount * lemonadePrice;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Hamburger Tutarı: "+totalHamburgerPrice+" TL");
+            Console.WriteLine("Pizza Tutarı: "+ totalPizzaPrice + " TL");
+            Console.WriteLine("Kızartma Tutarı: "+ totalFriesPrice + " TL");
+            Console.WriteLine("Kola Tutarı: "+ totalCokePrice + " TL");
+            Console.WriteLine("Limonata Tutarı: "+ totalLemonadePrice + " TL");
+            Console.WriteLine("Su Tutarı: "+ totalWaterPrice + " TL");
+
+            Console.WriteLine();
+
+            int totalPrice = totalCokePrice + totalFriesPrice + totalHamburgerPrice + totalLemonadePrice + totalPizzaPrice + totalWaterPrice;
+
+            Console.WriteLine("Toplam Tutar: " + totalPrice + " TL");
+
             #endregion
 
             Console.ReadKey();
