5b5f53e53a139a406bab5edf57004c185d5cbad5
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-11 22:44:59 +0300

    For Loop with Conditionals

--- CSharpEgitimKampi/04_Loops/Program.cs
+++ CSharpEgitimKampi/04_Loops/Program.cs
@@ -39,6 +39,50 @@ namespace _04_Loops
 
             #endregion
 
+            #region For Loop with Conditionals
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if(i%5==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    totalValue += i;
+            //}
+
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if(i%2==0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("-----");
+            //Console.WriteLine(totalValue);
+
+            //int counter = 0;
+
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if(i%7==0)
+            //        counter++;
+            //}
+
+            //Console.WriteLine(counter);
+
+            #endregion
             Console.ReadKey();
         }
     }
