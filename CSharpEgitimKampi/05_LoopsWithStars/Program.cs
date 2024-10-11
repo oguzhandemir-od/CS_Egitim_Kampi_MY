6af8c8c3e4bfa97da587e4bc132714f50ca4faa2
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-11 23:16:58 +0300

    Top and Bottom 10 Stars

---
+++ CSharpEgitimKampi/05_LoopsWithStars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Top and Bottom 10 Stars
+
+            for (int i = 0; i <=10; i++)
+            {
+                Console.WriteLine("*");
+            }
+
+            #endregion
+            
+            Console.ReadKey();
+        }
+    }
+}
