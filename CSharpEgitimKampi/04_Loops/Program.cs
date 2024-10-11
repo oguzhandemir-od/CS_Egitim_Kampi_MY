874e0a5a0d8065e6939c44e3447dff658df12980
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-11 22:38:54 +0300

    For Loop

---
+++ CSharpEgitimKampi/04_Loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Loop
+
+            //int i;
+
+            //for(i=1;i<=5;i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for(int i=1;i<=20;i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for(int i=3;i<=50;i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adeti giriniz: ");
+            //int finishValue=int.Parse(Console.ReadLine());
+
+            //for(int i=1;i<=finishValue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
