bf6aa2a0a4cd856e235270151227d7d6bf07c102
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-11 22:50:53 +0300

    While Loop

--- CSharpEgitimKampi/04_Loops/Program.cs
+++ CSharpEgitimKampi/04_Loops/Program.cs
@@ -95,6 +95,37 @@ namespace _04_Loops
             //}
 
             #endregion
+
+            #region While Loop
+
+            //int i = 1;
+
+            //while(i<=10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}           
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //        Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while(i<=10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+
+            //Console.WriteLine(sum);
+
+            #endregion
             Console.ReadKey();
         }
     }
