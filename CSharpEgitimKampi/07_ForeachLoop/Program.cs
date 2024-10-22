83c644e2ca5990df5bd83782c2f6e0ed36f18741
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-22 08:42:14 +0300

    Foreach Loop

--- CSharpEgitimKampi/07_ForeachLoop/Program.cs
+++ CSharpEgitimKampi/07_ForeachLoop/Program.cs
@@ -10,7 +10,43 @@ namespace _07_ForeachLoop
     {
         static void Main(string[] args)
         {
+            #region Foreach Loop
 
+            //string[] cities = { "Milano", "Roma", "Budapeşte", "Ankara", "İstanbul", "Varşova" };
+
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 285, 6578, 10394 };
+
+            //foreach(int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 285, 6578, 10394 };
+
+            //foreach(int number in numbers)
+            //{
+            //    if(number%2==0)
+            //        Console.WriteLine(number);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 285, 6578, 10394 };
+            //int total = 0;
+
+            //foreach(int i in numbers)
+            //{
+            //    total += i;
+            //}
+
+            //Console.WriteLine(total);
+
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
