3e81a1c522c18d8a48676313cbffd7ec808b90d2
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-22 09:49:07 +0300

    Void Methods without Parameters

---
+++ CSharpEgitimKampi/08_Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Methods without Parameters
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Hakan Öztürk");
+            //    Console.WriteLine("Merve Çınar");
+            //}
+
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+
+            //Sum();
+
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
