1421258af5f646b4ba7be4fc24530826fc9d8286
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-11-12 20:30:12 +0300

    CRUD - Create, Adding Category

---
+++ CSharpEgitimKampi/10_DatabaseCrud/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------------------------");
+            #region Adding Category
+            //Console.Write("Eklemek istediğiniz kategori adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-CR14I0T;Initial Catalog=EgitimKampiDb;" +
+            //    "Integrated Security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Insert Into TblCategory (CategoryName) values (@p1)", connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.Write("Kategori başarıyla eklendi!");
+            #endregion
+
+            Console.ReadKey();
+        }
+        
+    }
+}
